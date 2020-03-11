2034a22503220e6d4deba4ce371741a5db97d8a2
kadrianne <kristine.a.du@gmail.com>
2020-03-11 10:06:01 -0600

    Create authors actions.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,4 +8,20 @@ class AuthorsController < ApplicationController
     author = Author.find(params[:id])
     render json: author
   end
+
+  def create
+    author = Author.create(name: params[:name], age: params[:age])
+    render json: author
+  end
+
+  def update
+    author = Author.find(params[:id])
+    author.update(name: params[:name], age: params[:age])
+    render json: author
+  end
+
+  def destroy
+    Author.find(params[:id]).destroy
+    render json: {message: 'Author deleted successfully.'}
+  end
 end
